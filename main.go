8cee069cc111bcc8a28c8f9b557d784f231b27cd
Slowlyo <1821725017@qq.com>
2023-03-09 23:14:05 +0800

    update

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	checkDir()
+	generateFile()
+	time.Sleep(3 * time.Second)
+	gitHandler()
+}
+
+func generateFile() {
+	maintenanceQuantity()
+
+	timestamp := fmt.Sprintf("%x", md5.Sum([]byte(time.Now().String())))
+
+	os.Create("./files/" + timestamp)
+
+	log("create file: " + timestamp)
+}
+
+func maintenanceQuantity() {
+	files, err := ioutil.ReadDir("./files")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if len(files) > 10 {
+		for _, file := range files {
+			os.Remove("./files/" + file.Name())
+			log("remove file: " + file.Name())
+		}
+	}
+}
+
+func checkDir() {
+	if _, err := os.Stat("./files"); os.IsNotExist(err) {
+		os.Mkdir("./files", os.ModePerm)
+	}
+}
+
+func log(str string) {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + ": " + str)
+}
+
+func gitHandler() {
+	execCommand("git add .")
+	execCommand("git commit -m 'update'")
+	execCommand("git push")
+}
+
+func execCommand(command string) {
+	cmd := exec.Command("/bin/bash", "-c", command)
+	cmd.Dir = "./"
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
